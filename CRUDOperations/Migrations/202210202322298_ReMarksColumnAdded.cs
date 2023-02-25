1bb3d33f4d6e95f29193ce566f4596335e9f9c44
basemelazab <basem.elazab2014@gmail.com>
2023-02-25 02:40:34 +0200

    Add project files.

---
+++ CRUDOperations/Migrations/202210202322298_ReMarksColumnAdded.cs
@@ -0,0 +1,18 @@
+﻿namespace CRUDOperations.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ReMarksColumnAdded : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Products", "ReMarks", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Products", "ReMarks");
+        }
+    }
+}
---
+++ CRUDOperations/Models/Products.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CRUDOperations.Models
+{
+    public class Products
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required (ErrorMessage ="Product Name is Required.")]
+        [Display(Name ="Product Name")]
+        public string ProductName { get; set; }
+        [Required(ErrorMessage = "Price is Required.")]
+        [Display(Name = "Price")]
+        public decimal Price { get; set; }
+        [Required(ErrorMessage = "Quantity is Required.")]
+        [Display(Name = "Quantity")]
+        public int Quantity { get; set; }
+        [Required(ErrorMessage = "ReMarks is Required.")]
+        [Display(Name = "ReMarks")]
+        public String ReMarks { get; set; }
+    }
+    public class EFCodeFirstEntities:DbContext
+    {
+        public DbSet<Products> products { get; set; }
+    }
+}
